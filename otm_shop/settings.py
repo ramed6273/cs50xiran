eaa6ae32adbfee302125fe6f9f50f8a042f30b12
Amirhossein Dehghan <58779469+amirhosseindehghan1@users.noreply.github.com>
2021-12-09 13:47:17 +0330

    Update settings.py

--- otm_shop/settings.py
+++ otm_shop/settings.py
@@ -136,11 +136,11 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-# STATICFILES_DIRS = ['/my_app_dir/static',   os.path.join(BASE_DIR, 'static/')]
-# STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = ['/my_app_dir/static',   os.path.join(BASE_DIR, 'static/')]
 STATIC_URL = 'static/'
+
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
