e11b8861660ab476779f5660e5e7d4d5e3d845b5
amir <58779469+amirhosseindehghan1@users.noreply.github.com>
2022-01-06 16:42:41 +0330

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -48,13 +48,13 @@ urlpatterns = [
     # path('reset/<uidb64>/<token>', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     # path('password_reset_complete', views.PasswordResetCompleteView.as_view(), name='password_reset_complete')
     path("password_reset", views.password_reset_request, name="password_reset"),
-    path('password_reset/done/',
+    path("password_reset/done/",
          auth_views.PasswordResetDoneView.as_view(template_name='password/password_reset_done.html'),
          name='password_reset_done'),
-    path('reset/<uidb64>/<token>/',
+    path("reset/<uidb64>/<token>/",
          auth_views.PasswordResetConfirmView.as_view(template_name="password/password_reset_confirm.html"),
          name='password_reset_confirm'),
-    path('reset/done/',
+    path("reset/done/",
          auth_views.PasswordResetCompleteView.as_view(template_name='password/password_reset_complete.html'),
          name='password_reset_complete'),
 ]
