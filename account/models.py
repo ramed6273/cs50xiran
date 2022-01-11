7b0dd0611f1f90a2152e76c7ecc2aa4e9af170ea
amir <58779469+amirhosseindehghan1@users.noreply.github.com>
2022-01-11 15:18:28 +0330

    Update models.py

--- account/models.py
+++ account/models.py
@@ -44,8 +44,8 @@ class User(AbstractBaseUser):
     city = models.CharField('شهر', max_length=100, null=True)
     rezume = models.CharField('لینک رزومه', max_length=300, null=True, blank=True)
     postal_code = models.PositiveIntegerField('کد پستی', null=True)
-    linked_in = models.CharField('لینکدین', max_length=100, null=True)
-    static_phone = models.PositiveIntegerField('تلفن ثابت', null=True)
+    linked_in = models.CharField('لینکدین', max_length=100, null=True, blank=True)
+    static_phone = models.PositiveIntegerField('تلفن ثابت', null=True, blank=True)
     USERNAME_FIELD = 'email'
     complete_profile = models.BooleanField('تکمیل پروفایل', default=False)
     REQUIRED_FIELDS = ['name', 'last_name', 'phone']
