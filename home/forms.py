0e76de23248b28252b0e17d3c86c5af66133985d
amir <58779469+amirhosseindehghan1@users.noreply.github.com>
2022-01-18 00:11:17 +0330

    Update forms.py

--- home/forms.py
+++ home/forms.py
@@ -18,3 +18,14 @@ class UpdateAccount(forms.ModelForm):
         self.fields["last_name"].disabled = True
         self.fields["email"].disabled = True
         self.fields["phone"].disabled = True
+# reset password
+class ResetPasswordForm(forms.Form):
+    old_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control', 'placeholder': 'Old Password'
+    }))
+    new_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control', 'placeholder': 'New Password'
+    }))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control', 'placeholder': 'Confirm Password'
+    }))
